8d5a3b69c021c9f2f99b6c035f8a6f4ba1d1a7b2
Sujoy Manna <62413879+sujoy-2811@users.noreply.github.com>
2025-12-07 06:14:55 +0530

    fix: wrong import

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,7 +1,6 @@
 import Footer from "./components/layout/Footer";
 import Header from "./components/layout/Header";
 import { Button } from "@/components/ui/button";
-import "@/";
 
 function App() {
   return (
